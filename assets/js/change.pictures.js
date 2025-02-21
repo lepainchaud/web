5c120514d5b200324cc0d79626dddc0b7210dfb9
lepainchaud <yann.prik@gmail.com>
2025-02-21 22:07:10 +0800

    Update change.pictures.js

--- assets/js/change.pictures.js
+++ assets/js/change.pictures.js
@@ -1,8 +1,8 @@
 // List of background images (can be URLs or paths to your images)
 const images = [
-    'url(../../images/banner.jpg)', 
-    'url(../../images/beef_rendang.png)', 
-    'url(../../images/burek.jpg)'
+    'url(../images/banner.jpg)', 
+    'url(../images/beef_rendang.png)', 
+    'url(../images/burek.jpg)'
 ]; 
 
 let currentIndex = 0; // Start with the first image
